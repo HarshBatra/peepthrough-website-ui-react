95db6ff9c9f30e321307322ec0d2531f015c8951
HARSH BATRA <harshbat22@gmail.com>
2022-12-30 19:10:43 +0530

    Updates

---
+++ precache-manifest.5157df65909d3ff7a4585e5791563b5d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b2d21c7c15ddbd94a34",
+    "url": "/peepthrough-website-ui-react/static/css/main.d3a0e235.chunk.css"
+  },
+  {
+    "revision": "7b2d21c7c15ddbd94a34",
+    "url": "/peepthrough-website-ui-react/static/js/main.7b2d21c7.chunk.js"
+  },
+  {
+    "revision": "8154e8f825dc95d164e5",
+    "url": "/peepthrough-website-ui-react/static/css/1.6790dad0.chunk.css"
+  },
+  {
+    "revision": "8154e8f825dc95d164e5",
+    "url": "/peepthrough-website-ui-react/static/js/1.8154e8f8.chunk.js"
+  },
+  {
+    "revision": "01528a775e7a524c7d6c",
+    "url": "/peepthrough-website-ui-react/static/js/runtime~main.01528a77.js"
+  },
+  {
+    "revision": "aac4a468f690dfeab939302b91b3e62d",
+    "url": "/peepthrough-website-ui-react/static/media/costume-clothes.aac4a468.png"
+  },
+  {
+    "revision": "4f8322f0f2df727a20e4619ad82f9c61",
+    "url": "/peepthrough-website-ui-react/static/media/baby-pacifier.4f8322f0.png"
+  },
+  {
+    "revision": "bca1137eec3fa456f5b0466fcd93847a",
+    "url": "/peepthrough-website-ui-react/static/media/necklace.bca1137e.png"
+  },
+  {
+    "revision": "6a055b79b931a8545a6e64df8edcd4eb",
+    "url": "/peepthrough-website-ui-react/static/media/fast-food.6a055b79.png"
+  },
+  {
+    "revision": "2aca9948c401c673c846e1e6c5c3dc25",
+    "url": "/peepthrough-website-ui-react/static/media/makeup.2aca9948.png"
+  },
+  {
+    "revision": "8bbe5815a179883bbf6442fe2c52543e",
+    "url": "/peepthrough-website-ui-react/static/media/plug.8bbe5815.png"
+  },
+  {
+    "revision": "1c50228dcfdd290ceb51c2fceff93673",
+    "url": "/peepthrough-website-ui-react/static/media/screwdriver.1c50228d.png"
+  },
+  {
+    "revision": "d3d4b0604e2944d5557af3a3268f4f1f",
+    "url": "/peepthrough-website-ui-react/static/media/real-estate.d3d4b060.png"
+  },
+  {
+    "revision": "3e1423f8185e97665079b07f4c0ddcf6",
+    "url": "/peepthrough-website-ui-react/static/media/3d-glasses.3e1423f8.png"
+  },
+  {
+    "revision": "0d8c22750ca88447b302078c84cd7691",
+    "url": "/peepthrough-website-ui-react/static/media/stethoscope.0d8c2275.png"
+  },
+  {
+    "revision": "c00a5335ab3f49d7dd6f438b143a45be",
+    "url": "/peepthrough-website-ui-react/static/media/gift.c00a5335.png"
+  },
+  {
+    "revision": "8a96b3770022e6335f69d3569552f6a2",
+    "url": "/peepthrough-website-ui-react/static/media/stationery.8a96b377.png"
+  },
+  {
+    "revision": "2f4eee1d80f5b78ff36ca65077ea0cf2",
+    "url": "/peepthrough-website-ui-react/static/media/bank.2f4eee1d.png"
+  },
+  {
+    "revision": "da9647df3e7d59e2c8073a69ab1f7ecc",
+    "url": "/peepthrough-website-ui-react/static/media/chair.da9647df.png"
+  },
+  {
+    "revision": "e9cb65436ada32e16b35dcd7be4faccd",
+    "url": "/peepthrough-website-ui-react/static/media/basketball.e9cb6543.png"
+  },
+  {
+    "revision": "fa94a4623318c8e08c8c81229109b74c",
+    "url": "/peepthrough-website-ui-react/static/media/popcorn.fa94a462.png"
+  },
+  {
+    "revision": "1347187d71405b359de6dd2a88fde8d7",
+    "url": "/peepthrough-website-ui-react/static/media/hotel.1347187d.png"
+  },
+  {
+    "revision": "e933dee7b695d5555c9bd4dcb783fa14",
+    "url": "/peepthrough-website-ui-react/static/media/miscellaneous.e933dee7.png"
+  },
+  {
+    "revision": "99358a7b08e8f652c551d8b9d3f52c1e",
+    "url": "/peepthrough-website-ui-react/static/media/data-searching.99358a7b.png"
+  },
+  {
+    "revision": "3d19c01b1e9f41bec393fb86c39d13b7",
+    "url": "/peepthrough-website-ui-react/static/media/location.3d19c01b.png"
+  },
+  {
+    "revision": "f9dd6e6728eeecce7890c2de411b38e4",
+    "url": "/peepthrough-website-ui-react/static/media/sale.f9dd6e67.png"
+  },
+  {
+    "revision": "ebfebc4c1b6dd71c7193c8449bd18483",
+    "url": "/peepthrough-website-ui-react/static/media/banglore.ebfebc4c.jpg"
+  },
+  {
+    "revision": "44342b5170d1cf2d5c6875c0afdcdf18",
+    "url": "/peepthrough-website-ui-react/static/media/gurugram.44342b51.jpg"
+  },
+  {
+    "revision": "1e1030d0277ef808d0a7d6a3656acca8",
+    "url": "/peepthrough-website-ui-react/static/media/noida.1e1030d0.jpg"
+  },
+  {
+    "revision": "84d3aa650cc5dbd3b2b2c3ed3f1d836b",
+    "url": "/peepthrough-website-ui-react/static/media/mumbai.84d3aa65.jpg"
+  },
+  {
+    "revision": "0c536f1f6dcad4e93cb872411d542252",
+    "url": "/peepthrough-website-ui-react/static/media/delhi.0c536f1f.jpg"
+  },
+  {
+    "revision": "1279fd115672d7b9460237226120d0b1",
+    "url": "/peepthrough-website-ui-react/static/media/bg.1279fd11.jpg"
+  },
+  {
+    "revision": "ab0f4abccd1f3a1723165970ae94107f",
+    "url": "/peepthrough-website-ui-react/index.html"
+  }
+];
